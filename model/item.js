8408f4a9002e97b2cb5a1ac0d38776bfad71c864
Catherine <cmlavin7@gmail.com>
2017-07-31 16:41:23 -0400

    added constructors

---
+++ model/item.js
@@ -0,0 +1,12 @@
+function createItem() {
+  let itemID = 0
+  return class {
+    constructor(name, description, picture) {
+      this.name = name;
+      this.description = description;
+      this.picture = picture;
+      this.id = ++itemID;
+      store.items.push(this);
+    }
+  }
+}
---
+++ model/user.js
@@ -0,0 +1,13 @@
+function createUser() {
+  let userID = 0
+  return class {
+    constructor(name, location, rating, email) {
+      this.name = name;
+      this.location = location;
+      this.rating = rating;
+      this.email = email;
+      this.id = ++itemID;
+      store.users.push(this);
+    }
+  }
+}
