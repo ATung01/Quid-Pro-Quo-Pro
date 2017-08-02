ae0fe8402b98b4349565d004f02fa5e20edca242
Catherine <cmlavin7@gmail.com>
2017-08-02 14:52:58 -0400

    added event listeners

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-git$(function() {
-submitForm();
+$(function() {
+login();
+buy();
+sell();
+itemInfo();
 
 $('.carousel').carousel();
 
 
-
-
-
-
-
-
-
 });
 
-function submitForm() {
+function login() {
   $("#login-button").on("click", function(event) {
-      event.preventDefault()
+      event.preventDefault();
       // $('#login-form').empty();
       // $('#login-form').append("<h5>These are your posts:</h5>")
-      window.location.replace('post.html')
+      //window.location.replace('post.html')
   })
+}
+
+function buy() {
+  $("#buy-button").on("click", function(event) {
+    event.preventDefault();
+  })
+}
+
+function sell() {
+  $("#sell-button").on("click", function(event) {
+    event.preventDefault();
+}
+
+function itemInfo() {
+  $(`#modal${item.id}`).on("click", function(event) {
+    event.preventDefault();
 }
