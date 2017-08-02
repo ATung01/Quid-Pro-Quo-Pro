302327b1b407cccb5ac0ad36bc7cc94c67a6baf4
github username <github email address>
2017-08-02 10:23:18 -0400

    pushing posts

--- model/item.js
+++ model/item.js
@@ -20,12 +20,18 @@ function createItem() {
         return item.id !== itemID
       })
     }
+    renderPicture(){
+      return `<a class="carousel-item btn modal-trigger" data-target="modal1" href="#modal1"><img src="../photos/${this.picture}"></a>`
+    }
 
   }
 }
 
 let Item = createItem()
 
-let i1 = new Item ("TV", "It's a tv.", "picture of tv")
-let i2 = new Item ("Boombox", "For oldschool cool", "picture of boombox")
-let i3 = new Item ("Yugioh Cards", "Believe in the heart of the cards.", "picture of yugioh cards")
+let i1 = new Item ("TV", "It's a tv.", "10317673.jpg")
+let i2 = new Item ("Boombox", "For oldschool cool", "boombox.jpeg")
+let i3 = new Item ("Yugioh Cards", "Believe in the heart of the cards.", "maxresdefault.jpg")
+let i4 = new Item ("Painting", "It's worth a bit", "painting.webp")
+let i5 = new Item ("Nail Gun", "Pew Pew Pew", "nailgun.webp")
+let i6 = new Item ("backpack", "A pack for backs", "backpack.jpg")
--- posts.js
+++ posts.js
@@ -2,6 +2,18 @@ $(function() {
 
  $('.modal').modal();
 
+let slider = $('.carousel')
+Item.showAll().forEach( function(item) {
+  slider.append(item.renderPicture())
+
+})
+
+if (slider.hasClass('initialized')){
+    slider.removeClass('initialized')
+}
+
+//just reinit the carousel
+slider.carousel();
 
 
 
