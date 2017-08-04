266fda8c9be1be6cd50577643a63f479886ec909
github username <github email address>
2017-08-04 11:14:09 -0400

    pushing to master

--- itemsAdapter.js
+++ itemsAdapter.js
@@ -7,5 +7,18 @@ class ItemAdapter {
       .then( y => console.log(y) )
   }
 
+  static makeItem(name, desc, pic, user_id){
+    let data = new FormData();
+    data.append( "name", JSON.stringify(name));
+    data.append( "description", JSON.stringify(desc));
+    data.append( "picture", JSON.stringify(pic));
+    data.append( "user_id", JSON.stringify(user_id));
+    let myHeaders = new Headers()
+    myHeaders["Content-Type"] = "application/json"
+
+    return fetch(`http://localhost:3000/item`, {method: 'post', headers: myHeaders, body: data}).then(resp => resp.json())
+
+  }
+
 
 }
